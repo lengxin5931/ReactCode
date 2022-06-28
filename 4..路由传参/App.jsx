f95f4d022effe1a5927a2c2c4e839c2ccdfbd875
lengxin <593156599@qq.com>
2022-06-28 15:11:05 +0800

    路由传参

--- 4..路由传参/App.jsx
+++ 4..路由传参/App.jsx
@@ -6,18 +6,30 @@ import Sixin from "./pages/Sixin";
 import WoDe from "./pages/WoDe";
 
 export default class App extends Component {
+  state = {
+    id: 1,
+  };
   render() {
     return (
       <div className="App">
         <Route path="/home" component={Home}></Route>
+        <Route path="/sixin/:id" component={Sixin}></Route>
         <Route path="/wode" component={WoDe}></Route>
-        <Route path="/sixin" component={Sixin}></Route>
-        <Route path="/" exact component={Login}></Route>
+        {/* <Route path="/" exact component={Login}></Route> */}
 
         <footer>
           <Link to="/home">首页</Link>
-          <Link to="/sixin">私信</Link>
-          <Link to="/wode">我的</Link>
+          <Link to={`/sixin/${this.state.id}`}> 私信</Link>
+          <li
+            onClick={() => {
+              this.props.history.push({
+                pathname: "/wode",
+                search: "?id=1&name=zhangsan",
+              });
+            }}
+          >
+            我的
+          </li>
         </footer>
       </div>
     );
--- 4..路由传参/pages/Sixin.jsx
+++ 4..路由传参/pages/Sixin.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 export default class Sixin extends Component {
   render() {
+    console.log(this);
     return <div>Sixin</div>;
   }
 }
